d3cfcd959cccf660f79bc87993c9342a7331f1fb
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2022-01-27 17:34:28 -0600

    Update basic-beyblade.js

--- scripts/unit/beyblades/basic-beyblade.js
+++ scripts/unit/beyblades/basic-beyblade.js
@@ -1,9 +1,8 @@
-const basicBayBlade = extend(UnitType, "name", {
-  spinSpeed: 2,
-  clockwise: -1,
-  health: 100,
-  speed: 3,
+const unit = extend(UnitType, "basic-bayblade.json", {
   update(unit){
     this.super$update(unit);
     unit.vel().rotate(unit.rotation + Time.delta * this.spinSpeed * this.clockwise);
   }
+});
+unit.constructor = () => extend(MechUnit, {});
+unit.defaultController = () => extend(SuicideAi, {});
